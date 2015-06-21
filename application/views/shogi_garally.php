22216b29d9b5da3c7c7a3397c90af3d18a2f230b
Ko <knockoutmarch@gmail.com>
2015-06-21 15:35:04 +0900

    modify script path

--- application/views/shogi_garally.php
+++ application/views/shogi_garally.php
@@ -115,7 +115,7 @@ endforeach;
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="/assets/js/bootstrap.min.js"></script>
 
 <!-- Analytics -->
 <script>
--- application/views/shogi_show.php
+++ application/views/shogi_show.php
@@ -134,7 +134,7 @@
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="/assets/js/bootstrap.min.js"></script>
 
 <!-- Analytics -->
 <script>
